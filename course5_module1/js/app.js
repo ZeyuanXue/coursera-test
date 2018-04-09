4472ae3b4525e18b16e3eabfff4c9389d17454ec
ZeyuanXue <zeyuanxue@gmail.com>
2018-04-09 13:09:34 +0800

    finished

---
+++ course5_module1/js/app.js
@@ -0,0 +1,30 @@
+(function (){
+  'use strict;'
+  angular.module('LunchCheck',[]).controller('LunchCheckController', LunchCheckController);
+  LunchCheckController.$inject = ['$scope'];
+
+  function LunchCheckController($scope){
+    $scope.msg = "";
+    $scope.in = "";
+
+    $scope.check = function (){
+      var temp = checkNum($scope.in);
+      //console.log($scope.in);
+      if (temp>0 && temp<=3){
+        $scope.msg = "Enjoy!";
+      }else if (temp>3){
+        $scope.msg = "Too Much!";
+      }
+    }
+
+  }
+
+  function checkNum (str){
+    if (str==""){
+      return 0;
+    }else{
+      var arr = str.split(',');
+      return arr.length;
+    }
+  }
+})();
